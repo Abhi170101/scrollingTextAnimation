d85cf30faf5a2ff3621dba22c1dd675f167aff25
Abhi170101 <abhilasha.wms@gmail.com>
2024-05-16 12:14:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function marqueAnimation(){
+    window.addEventListener("wheel",function(dets){
+        if(dets.deltaY>0){
+            gsap.to(".marque",{
+                transform:"translateX(-100%)",
+                duration:2,
+                repeat:-1,
+                ease:"none"
+            })
+            gsap.to(".marque img",{
+                rotate:180,
+            })
+        }
+            else{
+                gsap.to(".marque",{
+                    transform:"translateX(0%)",
+                    duration:2,
+                    repeat:-1,
+                    ease:"none"
+            })
+            gsap.to(".marque img",{
+                rotate:0,
+            })
+        }
+        
+    })
+}
+
+marqueAnimation()
+
+
